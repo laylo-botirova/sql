68a7f700ef882fab268e2a61872c44956c9f1aa6
botirovalaylo567 <botirovalaylo567@gmail.com>
2025-10-24 05:54:06 -0700

    test

--- lessons/src/main/java/org/example/sql/lesson1/TestClass.java
+++ lessons/src/main/java/org/example/sql/lesson1/TestClass.java
@@ -1,8 +1,8 @@
-package org.example.lessons.lesson1;
+package org.example.sql.lesson1;
 
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+
     }
 }
